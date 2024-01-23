5a34ce3836b6662cc525b54ea3339b7e76f6b7ca
djimi <djimi9530@gmail.com>
2024-01-23 13:33:24 +0100

    Url crud

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
-from tabledef import *
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
-db = SQLAlchemy(app)
-
-engine = create_engine('sqlite:///student.db', echo=False)
+engine = create_engine('sqlite:///url.db', echo=False)
 # create a Session
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with app.app_context():
-    db.create_all()
+Base = declarative_base()
+
+class Url(Base):
+    """"""
+    __tablename__ = "url"
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    description = Column(String)
+    category = Column(String)
+
+    def __init__(self, url, description, category):
+        self.url = url
+        self.description = description
+        self.category = category
+
+
+# create tables
+Base.metadata.create_all(engine)
+
 
 @app.route("/")
 def index():
-    students = session.query(Student).all()
-    # students = Student.query.all()
-    return render_template("index.html", students=students)
+    urls = session.query(Url).all()
+    return render_template("index.html", urls=urls)
 
 
 @app.route('/new', methods=['GET', 'POST'])
@@ -28,55 +42,51 @@ def create():
     if request.method == 'GET':
         return render_template('create.html')
     if request.method == 'POST':
-        username = request.form["username"]
-        firstname = request.form["firstname"]
-        lastname = request.form["lastname"]
-        university = request.form["university"]
-        student = Student(username=username, firstname=firstname, lastname=lastname, university=university)
-        session.add(student)
+        url = request.form["url"]
+        description = request.form["description"]
+        category = request.form["category"]
+        url = Url(url=url, description=description, category=category)
+        session.add(url)
         session.commit()
         return redirect('/')
 
 
 @app.route("/<int:id>")
 def show(id):
-    # student = Student.query.get(id)
-    student = session.get(Student, id)
-    return render_template("show.html", student=student)
+    url = session.get(Url, id)
+    return render_template("show.html", url=url)
 
 @app.route("/update/<int:id>", methods = ['GET','POST'])
 def update(id):
-    student = session.get(Student, id)
+    url = session.get(Url, id)
     if request.method == 'POST':
-        if student:
-            session.delete(student)
+        if url:
+            session.delete(url)
             session.commit()
- 
-            username = request.form["username"]
-            firstname = request.form["firstname"]
-            lastname = request.form["lastname"]
-            university = request.form["university"]
-            student = Student(username=username, firstname=firstname, lastname=lastname, university=university)
- 
-            session.add(student)
+
+            url = request.form["url"]
+            description = request.form["description"]
+            category = request.form["category"]
+            url = Url(url=url, description=description, category=category)
+            session.add(url)
             session.commit()
             return redirect('/')
-        return f"Student with id = {id} Does not exist"
+        else:
+            return f"Url with id = {id} Does not exist"
  
-    return render_template('update.html', student = student)
+    return render_template('update.html', url=url)
 
 
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete(id):
-    student = session.get(Student, id)
+    url = session.get(Url, id)
     if request.method == 'POST':
-        if student:
-            session.delete(student)
+        if url:
+            session.delete(url)
             session.commit()
             return redirect('/')
-        # abort(404)
 
-    return render_template('delete.html', student=student)
+    return render_template('delete.html', url=url)
 
 
 if __name__ == '__main__':
